d647e8febf8b79f1f02dfce1efc8bbee1dc6bf54
Vo Kim Thanh <thanh.vo@libnero.com>
2022-05-30 17:03:12 +0700

    page config

---
+++ view/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<div className="w-screen h-screen bg-gradient-to-r from-blue-400 to-blue-600 flex items-center justify-center p-4">
+			<div className="w-full max-w-md min-h-500 bg-white shadow shadow-gray-700 rounded p-2"></div>
+		</div>
+	);
+};
+
+export default HomePage;
